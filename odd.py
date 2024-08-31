b36bf9ad27b3b3b821aa9f1d6ede92a502819104
Siddharth Agrawal <137896685+siddharthag24@users.noreply.github.com>
2024-08-31 10:12:53 +0530

    Create odd.py

---
+++ odd.py
@@ -0,0 +1,15 @@
+dict1{ }
+def Contacts(name,phone,email):
+    dict1.append(name)
+    dict1.append(phone)
+    dict1.append(email)
+
+def add_contacts(name,phone,email):
+    {"Sid",98,sam}
+    {"manav",96,samp}
+    {"yash",95,sample}
+
+def Update(name,phone,email):
+    {"abc",98,sam1}
+    {"bac",96,sam2}
+    {"cab",95,sam3}
